27b9b1b61f0a5a0707e47bd21292fa1438efab1d
Yousuf Rashid <yousufrashid.coding@gmail.com>
2025-11-28 04:54:14 -0500

    changed the searchbar player database

--- backend/app/database/all_players.py
+++ backend/app/database/all_players.py
@@ -1,4 +1,4 @@
-from nba_api.stats.static.players import get_players
+from nba_api.stats.endpoints import leaguedashplayerstats
 
 _cached_players = None
 
@@ -6,7 +6,8 @@ def return_all_players():
     global _cached_players
 
     if _cached_players is None:
-        raw = get_players()
-        _cached_players = [p["full_name"] for p in raw]
+        season_stats = leaguedashplayerstats.LeagueDashPlayerStats(season='2025-26').get_data_frames()[0]
+        top100_df = season_stats[season_stats["NBA_FANTASY_PTS_RANK"] < 201]
+        top_100_players_names = top100_df["PLAYER_NAME"].tolist()
 
-    return _cached_players
+    return top_100_players_names
